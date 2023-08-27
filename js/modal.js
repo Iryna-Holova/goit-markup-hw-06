6bf73e114d27c9c661c826520d2b25158909a818
Iryna Holova <gis.golova@gmail.com>
2023-08-27 03:01:37 +0300

    add adaptation

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,22 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.modal.addEventListener('click', closeModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  function toggleModal(e) {
+    refs.modal.classList.toggle('is-hidden');
+    document.querySelector('html').classList.toggle('modal-show');
+  }
+
+  function closeModal(e) {
+    if (e.target !== e.currentTarget) return;
+    refs.modal.classList.add('is-hidden');
+    document.querySelector('html').classList.remove('modal-show');
   }
 })();
